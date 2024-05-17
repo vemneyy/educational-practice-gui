e25db436bb794044148b36ab86acd18b909648ba
Semyon Saprykin <saprykinsemen@gmail.com>
2024-05-17 13:51:18 +0300

    update

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+import os
 import sys
 
 from PyQt6 import uic, QtWidgets
@@ -6,23 +8,32 @@ from PyQt6.QtWidgets import QMainWindow
 
 
 class TheoryWindow(QMainWindow):
-    def __init__(self, theory_number, total_pages, window_name):
+    def __init__(self, theory_number, window_name):
         super().__init__()
         uic.loadUi('ui/theory_template.ui', self)
         self.main_window = MainWindow()
         self.setWindowTitle(window_name)
 
         self.current_page = 1
-        self.total_pages = total_pages
         self.theory_number = theory_number
-
-        self.buttonBack.setEnabled(False)
+        self.total_pages = self.get_total_pages()
+        self.labelPage.setText(f"{self.current_page}/{self.total_pages}")
+        self.buttonQuiz.setVisible(False)
         self.buttonMain.clicked.connect(self.openMainWindow)
         self.buttonForward.clicked.connect(lambda: self.loadPage('next'))
         self.buttonBack.clicked.connect(lambda: self.loadPage('previous'))
         self.buttonPrint.clicked.connect(self.printDocument)
 
         self.loadTextFromFile()
+
+    def get_total_pages(self):
+        theory_path = f'theory/theory_{self.theory_number}'
+        if os.path.exists(theory_path):
+            files = os.listdir(theory_path)
+            page_files = [f for f in files if f.startswith('page_') and f.endswith('.html')]
+            total_pages = len(page_files)
+            return total_pages
+        return 0
 
     def openMainWindow(self):
         self.main_window.show()
@@ -54,6 +65,9 @@ class TheoryWindow(QMainWindow):
 
         if self.current_page == self.total_pages:
             self.buttonForward.setEnabled(False)
+            self.buttonQuiz.setVisible(True)
+            # self.update_user_cache()
+
         else:
             self.buttonForward.setEnabled(True)
 
@@ -62,11 +76,35 @@ class TheoryWindow(QMainWindow):
         else:
             self.buttonBack.setEnabled(True)
 
+        self.updatePageLabel()
+
+    def updatePageLabel(self):
+        self.labelPage.setText(f"{self.current_page}/{self.total_pages}")
+
     def printDocument(self):
         printer = QPrinter()
         dialog = QPrintDialog(printer, self)
         if dialog.exec() == QPrintDialog.DialogCode.Accepted:
             self.textBrowser.print(printer)
+
+    # def update_user_cache(self):
+    #     cache_file = 'user.cache'
+    #     cache_data = {}
+    #
+    #     if os.path.exists(cache_file):
+    #         try:
+    #             with open(cache_file, 'r', encoding='utf-8') as file:
+    #                 cache_data = json.load(file)
+    #         except json.JSONDecodeError:
+    #             cache_data = {}
+    #     else:
+    #         with open(cache_file, 'w', encoding='utf-8') as file:
+    #             json.dump({}, file)
+    #
+    #     cache_data[f'theory_{self.theory_number}'] = True
+    #
+    #     with open(cache_file, 'w', encoding='utf-8') as file:
+    #         json.dump(cache_data, file, ensure_ascii=False, indent=4)
 
 
 class MainWindow(QMainWindow):
@@ -75,14 +113,14 @@ class MainWindow(QMainWindow):
         self.theory_window = None
         uic.loadUi('ui/app.ui', self)
 
-        self.buttonBasics_1.clicked.connect(lambda: self.open_theory_window(1, 3, 'Basics'))
-        self.buttonBasics_2.clicked.connect(lambda: self.open_theory_window(2, 3, 'Basics'))
-        self.buttonBasics_3.clicked.connect(lambda: self.open_theory_window(3, 3, 'Basics'))
-        self.buttonBasics_4.clicked.connect(lambda: self.open_theory_window(4, 4, 'Basics'))
-        self.buttonBasics_5.clicked.connect(lambda: self.open_theory_window(5, 3, 'Basics'))
+        self.buttonBasics_1.clicked.connect(lambda: self.open_theory_window(1, 'Основы электричества'))
+        self.buttonBasics_2.clicked.connect(lambda: self.open_theory_window(2, 'Basics'))
+        self.buttonBasics_3.clicked.connect(lambda: self.open_theory_window(3, 'Basics'))
+        self.buttonBasics_4.clicked.connect(lambda: self.open_theory_window(4, 'Basics'))
+        self.buttonBasics_5.clicked.connect(lambda: self.open_theory_window(5, 'Магнетизм'))
 
-    def open_theory_window(self, theory_number, total_pages, window_name):
-        self.theory_window = TheoryWindow(theory_number, total_pages, window_name)
+    def open_theory_window(self, theory_number, window_name):
+        self.theory_window = TheoryWindow(theory_number, window_name)
         self.theory_window.show()
         self.close()
 
--- test-app.py
+++ test-app.py
@@ -1,34 +1,62 @@
 import unittest
+from unittest.mock import patch, mock_open
 
 from PyQt6.QtWidgets import QApplication
 
 from app import TheoryWindow
 
-app = QApplication([])
-
 
 class TestTheoryWindow(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.app = QApplication([])  # Создаем экземпляр QApplication для тестов
+
     def setUp(self):
-        """Создаём экземпляр TheoryWindow перед каждым тестом."""
-        self.theory = TheoryWindow()
+        self.theory_number = 1
+        self.window_name = 'Test Window'
+        self.theory_window = TheoryWindow(self.theory_number, self.window_name)
 
-    def test_initial_state(self):
-        """Тестируем начальное состояние окна теории."""
-        self.assertEqual(self.theory.current_page, 1)
-        self.assertEqual(self.theory.total_pages, 3)
-        self.assertEqual(self.theory.window_name, 'Теория')
-        self.assertFalse(self.theory.buttonRevert.isVisible())
+    @patch('os.path.exists')
+    @patch('os.listdir')
+    def test_get_total_pages(self, mock_listdir, mock_exists):
+        mock_exists.return_value = True
+        mock_listdir.return_value = ['page_1.html', 'page_2.html', 'not_a_page.txt']
 
-    def test_load_next_page(self):
-        """Тестируем загрузку следующей страницы."""
-        self.theory.loadNextPage()
-        self.assertEqual(self.theory.current_page, 2)
+        total_pages = self.theory_window.get_total_pages()
+        self.assertEqual(total_pages, 2)
 
-    def test_load_text_from_file(self):
-        """Тестируем загрузку текста из файла."""
-        self.theory.loadTextFromFile()
-        # Здесь вам нужно будет адаптировать тест под ожидаемый контент файла
-        self.assertNotEqual(self.theory.textBrowser.toHtml(), '')
+    @patch('builtins.open', new_callable=mock_open, read_data='<p>Test Content</p>')
+    def test_loadTextFromFile(self, mock_file):
+        self.theory_window.current_page = 1
+        self.theory_window.loadTextFromFile()
+
+        self.assertEqual(self.theory_window.textBrowser.toHtml(),
+                         '<html><head></head><body><p>Test Content</p></body></html>')
+
+    @patch('builtins.open', new_callable=mock_open)
+    def test_loadTextFromFile_file_not_found(self, mock_file):
+        mock_file.side_effect = FileNotFoundError
+
+        self.theory_window.current_page = 1
+        self.theory_window.loadTextFromFile()
+
+        self.assertIn('<p style=" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">Файл не найден.</p>', self.theory_window.textBrowser.toHtml())
+
+    def test_loadPage(self):
+        self.theory_window.total_pages = 3
+
+        self.theory_window.current_page = 1
+        self.theory_window.loadPage('next')
+        self.assertEqual(self.theory_window.current_page, 2)
+
+        self.theory_window.loadPage('previous')
+        self.assertEqual(self.theory_window.current_page, 1)
+
+        self.theory_window.current_page = 3
+        self.theory_window.loadPage('previous')
+        self.assertEqual(self.theory_window.current_page, 2)
+
+
 
 
 if __name__ == '__main__':
